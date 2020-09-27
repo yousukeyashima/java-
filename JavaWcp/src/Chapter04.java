7f5d82d4f213caa6e64164983d2681f02734b58a
八島洋介 <yashimayousuke@yashiMacBook-Air.local>
2020-09-27 23:01:51 +0900

    first commit

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,11 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int LUCKY_NUMBER = 7;
+		String name = "太郎";
+		System.out.println(name);
+		name = "山田";
+		System.out.println(name);
+		System.out.println(LUCKY_NUMBER);
+	}
+}
